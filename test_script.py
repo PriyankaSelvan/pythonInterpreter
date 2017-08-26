276d32463dac182ce8b2d8339a209482570ac9c3
Priyanka Selvan <priyanka.nessie@gmail.com>
2017-08-27 04:16:57 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -3,6 +3,7 @@ import sys
 
 output = Popen(["python", "python_interpreter.py", "ex1.py"], stdout=PIPE).communicate()[0]
 answers = output.split()
+print answers
 actual = ['4', '6', '10', '12']
 for i in range(0,4):
     if answers[i]!=actual[i]:
